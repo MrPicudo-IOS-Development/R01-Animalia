1950acf1e1e5871240dfd9982456e244264307a6
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-06-23 04:08:19 -0600

    Basic designs, constraints and alignments for the first elements of the app.
    I made the app icon, the background and 4 images for the app in Canva, and added them to the project with the proper constraints.

---
+++ Animalia/ViewController.swift
@@ -0,0 +1,18 @@
+/* ViewController.swift --> Animalia. Created by Miguel Torres on 23/06/23. */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    
+    
+}
+
