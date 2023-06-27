f5f6cf1e1589fb44cc6f2cd7f9dd2b2c3ea554c5
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-06-26 20:28:06 -0600

    Project finished
    We solve the buttons title issue, download the sounds for the app, and manage the text of the information label.

--- Animalia/ViewController.swift
+++ Animalia/ViewController.swift
@@ -1,18 +1,55 @@
 /* ViewController.swift --> Animalia. Created by Miguel Torres on 23/06/23. */
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
     
+    @IBOutlet weak var doggy: UIButton!
+    @IBOutlet weak var michi: UIButton!
+    @IBOutlet weak var blue: UIButton!
+    @IBOutlet weak var windy: UIButton!
+    @IBOutlet weak var infoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        doggy.layer.cornerRadius = 25
+        michi.layer.cornerRadius = 25
+        blue.layer.cornerRadius = 25
+        windy.layer.cornerRadius = 25
+        // Necessary if we are not check the clip to bounds option.
+        doggy.clipsToBounds = true
+        infoLabel.text = ""
     }
-
-
     
+    @IBAction func pressedButton(_ sender: UIButton) {
+        if let title = sender.currentTitle {
+            sender.alpha = 0.5
+            playSound(title)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                sender.alpha = 1.0
+            }
+            switch title {
+            case "Doggy": infoLabel.text = "Dogs bark"
+            case "Michi": infoLabel.text = "Cats meow"
+            case "Blue": infoLabel.text = "Dolphins click and whistle"
+            case "Windy": infoLabel.text = "Birds chirp or tweet"
+            default: infoLabel.text = ""
+            }
+        } else {
+            print("The button doesn't have a title for it's current state.")
+        }
+    }
     
+    func playSound(_ name: String) {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "mp3") else {
+            print("Audio file not found.")
+            return
+        }
+        player = try! AVAudioPlayer(contentsOf: url)
+        player.play()
+    }
 }
 
